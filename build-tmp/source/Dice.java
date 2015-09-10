c78a0c67577f6fa2b4a66bfd1fb1baca001f32e3
Daniel <danielho54@gmail.com>
2015-09-10 11:40:22 -0700

    all

--- build-tmp/source/Dice.java
+++ build-tmp/source/Dice.java
@@ -1,10 +1,26 @@
-void setup()
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Dice extends PApplet {
+
+public void setup()
 {
 	size(610,610);
 	background(255, 255, 255);
 	noLoop();
 }
-void draw()
+public void draw()
 {
 	int y1;
 	int x1;
@@ -17,7 +33,7 @@ void draw()
 			x1=10;
 		}
 }
-void mousePressed()
+public void mousePressed()
 {
 	redraw();
 }
@@ -33,12 +49,12 @@ class Die //models one single dice cube
 		myY=y;
 
 	}
-	void roll()
+	public void roll()
 	{
 		number=(int)(Math.random()*6+1);
 	}
 	
-	void show()
+	public void show()
 	{
 		fill(0);
 		rect(myX, myY, 30, 30);
@@ -52,9 +68,9 @@ class Die //models one single dice cube
 					ellipse(myX+20,myY+20,5,5);
 					break;
 				case 3:
-					ellipse(myX+7.5,myY+7.5,5,5);
+					ellipse(myX+7.5f,myY+7.5f,5,5);
 					ellipse(myX+15,myY+15,5,5);
-					ellipse(myX+22.5,myY+22.5,5,5);
+					ellipse(myX+22.5f,myY+22.5f,5,5);
 					break;
 				case 4:
 					ellipse(myX+10,myY+10,5,5);
@@ -70,13 +86,22 @@ class Die //models one single dice cube
 					ellipse(myX+15,myY+15,5,5);
 					break;
 				case 6:
-					ellipse(myX+10,myY+7.5,5,5);
+					ellipse(myX+10,myY+7.5f,5,5);
 					ellipse(myX+10,myY+15,5,5);
-					ellipse(myX+10,myY+22.5,5,5);
-					ellipse(myX+20,myY+7.5,5,5);
+					ellipse(myX+10,myY+22.5f,5,5);
+					ellipse(myX+20,myY+7.5f,5,5);
 					ellipse(myX+20,myY+15,5,5);
-					ellipse(myX+20,myY+22.5,5,5);
+					ellipse(myX+20,myY+22.5f,5,5);
 					break;
 			}
 	}
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Dice" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
